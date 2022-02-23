88d718acc0ee96d05f3549d39ed3aafd595d9582
Chinese-guy-wr <tjuwrr@gmail.com>
2022-02-23 15:16:31 +0000

    good night

--- mainpage/views.py
+++ mainpage/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('WELCOMW TO BROTHERHOOD from ' + request.META['REMOTE_ADDR'])
+    context = {'ip': request.META['REMOTE_ADDR']}
+    return render(request, 'mainpage/index.html', context=context)
